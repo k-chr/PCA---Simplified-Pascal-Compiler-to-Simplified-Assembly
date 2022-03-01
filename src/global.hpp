776a8f0257324d571d9fa9bf6560a3cc49ad43fc
Kamil Chrustowski <chrustek123456789@gmail.com>
2022-03-01 03:22:24 +0100

    Added basic lexer.

--- src/global.hpp
+++ src/global.hpp
@@ -1,1 +1,20 @@
-#include "types/symbol.hpp"
+#include "types/compiler.hpp"
+
+typedef struct YYSTYPE YYSTYPE;
+struct YYSTYPE
+{
+	int int_val;
+	token token_val;
+};
+# define YYSTYPE_IS_TRIVIAL 0
+# define YYSTYPE_IS_DECLARED 1
+
+//gen/lexer
+int yylex();
+int yylex_destroy();
+
+//gen/parser
+int yyparse();
+void yyerror(char const* s);
+
+static int lineno = 0;
---
+++ src/types/emitter.hpp
@@ -0,0 +1,10 @@
+#include "symtable.hpp"
+#include <map>
+#include <string>
+
+class Emitter
+{		
+	private:
+		const static std::map<opcode, std::string> mnemonics;
+};
+
